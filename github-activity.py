0d4074a06d6a9afe965b787a2671dd0f47d3cb9a
Caleb Bucci <booch2nd@gmail.com>
2024-10-08 00:53:35 -0400

    Fixed requirements.txt and added some event activities

--- github-activity.py
+++ github-activity.py
@@ -11,13 +11,48 @@ def fetchGithubActivity(username: str):
     response = requests.get(api_url)
     if response.status_code == 200:
         events = response.json()
-        with open("temp.json", "w") as file:
+        with open("events.json", "w") as file:
             file.write(json.dumps(events, indent=4))
         
         print("Output:")
         for event in events:
-            if event['type'] == 'PushEvent':
-                pass
+            print("- ",end="")
+            if event['type'] == 'CommitCommentEvent':
+                print(f"Created comment on commit in {event['repo']['name']}")
+            elif event['type'] == 'CreateEvent':
+                print(f"Created {event['payload']['ref_type']} in {event['repo']['name']}")
+            elif event['type'] == 'DeleteEvent':
+                print(f"Deleted {event['payload']['ref_type']} in {event['repo']['name']}")
+            elif event['type'] == 'ForkEvent':
+                print(f"Forked {event['repo']['name']} to {event['payload']['forkee']['full_name']}")
+            elif event['type'] == 'GollumEvent':
+                print(f"Updated wiki pages in {event['repo']['name']}")
+            elif event['type'] == 'IssueCommentEvent':
+                print(f"{event['payload']['action'].capitalize()} comment on issue #{event['payload']['issue']['number']} in {event['repo']['name']}")
+            elif event['type'] == 'IssuesEvent':
+                print(f"{event['payload']['action'].capitalize()} an issue in {event['repo']['name']}")
+            elif event['type'] == 'MemberEvent':
+                print(f"{event['payload']['action'].capitalize()} a member in {event['repo']['name']}")
+            elif event['type'] == 'PublicEvent':
+                print(f"Made {event['repo']['name']} public")
+            elif event['type'] == 'PullRequestEvent':
+                
+            elif event['type'] == 'PullRequestReviewEvent':
+
+            elif event['type'] == 'PullRequestReviewCommentEvent':
+
+            elif event['type'] == 'PullRequestReviewThreadEvent':
+
+            elif event['type'] == 'PushEvent':
+
+            elif event['type'] == 'ReleaseEvent':
+
+            elif event['type'] == 'SponsorshipEvent':
+
+            elif event['type'] == 'WatchEvent':
+
+            else:
+                print(f"User {username} : {event['type']}")
 
     else:
         print(f"Status code: {response.status_code}")
